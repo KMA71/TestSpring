9f683b4b0178f437f0233868fb701b1e2f8e579f
Курдюков М.А <Kurdyukov_MA@rnb.local>
2019-09-12 17:43:44 +0300

    initial commit

---
+++ src/main/java/system/service/UserService.java
@@ -0,0 +1,19 @@
+package system.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import system.dao.UserDao;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    public List getAllUsers() {
+        return userDao.getAllUsers();
+    }
+}
